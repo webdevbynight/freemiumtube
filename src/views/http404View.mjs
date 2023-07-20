b53f4a9d584f99219595ddd6274090b72ee82575
webdevbynight <contact@victor-brito.name>
2023-07-20 16:44:54 +0200

    Added 404 body view

---
+++ src/views/http404View.mjs
@@ -0,0 +1,13 @@
+const http404View = () =>
+    {
+        const template =
+`
+            <section>
+                <h2>404</h2>
+                <p>404</p>
+            </section>
+`;
+            return template;
+    };
+
+export default http404View;
