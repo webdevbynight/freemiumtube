f38b3d304450c3b81fa8c3efc285708cb630e2d1
webdevbynight <contact@victor-brito.name>
2023-07-20 21:35:58 +0200

    Made account home page ready for i18n

--- src/controllers/accountController.mjs
+++ src/controllers/accountController.mjs
@@ -26,6 +26,7 @@ const getPage = async (req, res) =>
             },
             accountViewParams =
             {
+                lang,
                 myAccount
             };
 
--- src/views/accountView.mjs
+++ src/views/accountView.mjs
@@ -1,14 +1,26 @@
+import fs from 'node:fs';
+import gettext from 'gettext.js';
+
+import indexControllers from '../controllers/index.mjs';
+
 const accountView = (dynamicValues) =>
     {
-        const { myAccount } = dynamicValues,
-            template =
+        const { lang, myAccount } = dynamicValues,
+            i18n = gettext(),
+            __dirname = indexControllers.getDirname('.'),
+            json = fs.readFileSync(`${__dirname}/../i18n/locales/${lang}/account.json`, 'utf-8');
+        
+        i18n.setLocale(lang);
+        i18n.loadJSON(json);
+
+        const template =
 `
             <section class="back-office-account">
                 <h2>${myAccount}</h2>
                 <ul class="dashboard">
-                    <li><a href="/account/info">Mes informations</a></li>
-                    <li><a href="/account/videos">Mes vidéos</a></li>
-                    <li><a href="/account/delete">Supprimer mon compte</a></li>
+                    <li><a href="/account/info">${i18n.__('My information')}</a></li>
+                    <li><a href="/account/videos">${i18n.__('My videos')}</a></li>
+                    <li><a href="/account/delete">${i18n.__('Delete my account')}</a></li>
                 </ul>
             </section>
 `;
